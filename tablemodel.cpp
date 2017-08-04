6dfa3149c85580b9017994fb6c1e2e921017c638
Maxim Mikheev <mikheev_mp@nniirt.ru>
2017-08-04 14:39:22 +0300

    Переработка табличных моделей данных

--- tablemodel.cpp
+++ tablemodel.cpp
@@ -2,17 +2,161 @@
 #include <QColor>
 #include <QDebug>
 
-StrobeLengthModel::StrobeLengthModel(QObject *parent)
+/**
+ * @brief StrobeLengthWriteModel::StrobeLengthWriteModel
+ * @param parent
+ */
+
+StrobeLengthWriteModel::StrobeLengthWriteModel(QObject *parent)
     : QAbstractTableModel(parent), _rows(regims), _cols(strobs) {
 
-  for (int _row = 0; _row < _rows; ++_row)
-    for (int _col = 0; _col < _cols; ++_col) {
-      _data[_row][_col] = 0;
-      _flags[_row][_col] = false;
+  for (int row = 0; row < _rows; ++row)
+    for (int col = 0; col < _cols; ++col) {
+      _data[row][col] = 0;
     }
 }
 
-QVariant StrobeLengthModel::data(const QModelIndex &index, int role) const {
+/**
+ * @brief StrobeLengthWriteModel::data
+ * @param index
+ * @param role
+ * @return
+ */
+QVariant StrobeLengthWriteModel::data(const QModelIndex &index,
+                                      int role) const {
+  /**
+    Отображение данных в представлении
+  */
+  if (!index.isValid())
+    return QVariant();
+
+  if (role == Qt::TextAlignmentRole)
+    return Qt::AlignCenter;
+
+  return (role == Qt::DisplayRole || role == Qt::EditRole)
+             ? _data[index.row()][index.column()]
+             : QVariant();
+
+  return QVariant();
+}
+
+/**
+ * @brief StrobeLengthWriteModel::rowCount
+ * @param index
+ * @return
+ */
+int StrobeLengthWriteModel::rowCount(const QModelIndex &index) const {
+  /**
+    Возвращает количество строк в модели
+  */
+  Q_UNUSED(index)
+  return _rows;
+}
+
+/**
+ * @brief StrobeLengthWriteModel::columnCount
+ * @param index
+ * @return
+ */
+int StrobeLengthWriteModel::columnCount(const QModelIndex &index) const {
+  /**
+    Возвращает количество столбцов в модели
+  */
+  Q_UNUSED(index)
+  return _cols;
+}
+
+/**
+ * @brief StrobeLengthWriteModel::flags
+ * @param index
+ * @return
+ */
+Qt::ItemFlags StrobeLengthWriteModel::flags(const QModelIndex &index) const {
+  if (!index.isValid())
+    return Qt::ItemIsEnabled;
+
+  return QAbstractItemModel::flags(index) | Qt::ItemIsEditable;
+}
+
+/**
+ * @brief StrobeLengthWriteModel::setData
+ * @param index
+ * @param value
+ * @param role
+ * @return
+ */
+bool StrobeLengthWriteModel::setData(const QModelIndex &index,
+                                     const QVariant &value, int role) {
+  /**
+    Установка нового значения в ячейке при редактировании таблицы
+    */
+
+  if (index.isValid() && role == Qt::EditRole) {
+    _data[index.row()][index.column()] = value.value<int>();
+    emit dataChanged(index, index);
+    return true;
+  }
+
+  return false;
+}
+
+/**
+ * @brief StrobeLengthWriteModel::headerData
+ * @param section
+ * @param orientation
+ * @param role
+ * @return
+ */
+QVariant StrobeLengthWriteModel::headerData(int section,
+                                            Qt::Orientation orientation,
+                                            int role) const {
+  if (role == Qt::DisplayRole) {
+    if (orientation == Qt::Vertical) {
+      // reverse row numbers
+      return section;
+    }
+
+    if (orientation == Qt::Horizontal)
+      return section;
+  }
+
+  return QAbstractTableModel::headerData(section, orientation, role);
+}
+
+/**
+ * @brief StrobeLengthWriteModel::getModelData
+ */
+void StrobeLengthWriteModel::getModelData(quint16 (*data)[strobs]) {
+  /**
+    Берем данные, чтобы отослать их на сервер
+    */
+  for (int row = 0; row < _rows; ++row)
+    for (int col = 0; col < _cols; ++col)
+      data[row][col] = _data[row][col];
+}
+
+// Методы StrobeLengthReadModel
+/**
+ * @brief StrobeLengthReadModel::StrobeLengthReadModel
+ * @param parent
+ */
+StrobeLengthReadModel::StrobeLengthReadModel(QObject *parent)
+    : QAbstractTableModel(parent), _rows(regims), _cols(strobs) {
+
+  for (int row = 0; row < _rows; ++row)
+    for (int col = 0; col < _cols; ++col) {
+      _data[row][col] = 0;
+      _flags[row][col] = false;
+    }
+}
+
+/**
+ * @brief StrobeLengthReadModel::data
+ * @param index
+ * @param role
+ * @return
+ */
+QVariant StrobeLengthReadModel::data(const QModelIndex &index, int role) const {
   /**
     Отображение данных в представлении
   */
@@ -29,9 +173,16 @@ QVariant StrobeLengthModel::data(const QModelIndex &index, int role) const {
 
   return (role == Qt::DisplayRole) ? _data[index.row()][index.column()]
                                    : QVariant();
+
+  return QVariant();
 }
 
-int StrobeLengthModel::rowCount(const QModelIndex &index) const {
+/**
+ * @brief StrobeLengthReadModel::rowCount
+ * @param index
+ * @return
+ */
+int StrobeLengthReadModel::rowCount(const QModelIndex &index) const {
   /**
     Возвращает количество строк в модели
   */
@@ -39,7 +190,12 @@ int StrobeLengthModel::rowCount(const QModelIndex &index) const {
   return _rows;
 }
 
-int StrobeLengthModel::columnCount(const QModelIndex &index) const {
+/**
+ * @brief StrobeLengthReadModel::columnCount
+ * @param index
+ * @return
+ */
+int StrobeLengthReadModel::columnCount(const QModelIndex &index) const {
   /**
     Возвращает количество столбцов в модели
   */
@@ -47,30 +203,28 @@ int StrobeLengthModel::columnCount(const QModelIndex &index) const {
   return _cols;
 }
 
-Qt::ItemFlags StrobeLengthModel::flags(const QModelIndex &index) const {
+/**
+ * @brief StrobeLengthReadModel::flags
+ * @param index
+ * @return
+ */
+Qt::ItemFlags StrobeLengthReadModel::flags(const QModelIndex &index) const {
   if (!index.isValid())
     return Qt::ItemIsEnabled;
 
-  return QAbstractItemModel::flags(index) | Qt::ItemIsEditable;
+  return QAbstractItemModel::flags(index);
 }
 
-bool StrobeLengthModel::setData(const QModelIndex &index, const QVariant &value,
-                                int role) {
-  /**
-    Установка нового значения в ячейке при редактировании таблицы
-    */
-
-  if (index.isValid() && role == Qt::EditRole) {
-    _data[index.row()][index.column()] = value.value<int>();
-    emit dataChanged(index, index);
-    return true;
-  }
-
-  return false;
-}
-
-QVariant StrobeLengthModel::headerData(int section, Qt::Orientation orientation,
-                                       int role) const {
+/**
+ * @brief StrobeLengthReadModel::headerData
+ * @param section
+ * @param orientation
+ * @param role
+ * @return
+ */
+QVariant StrobeLengthReadModel::headerData(int section,
+                                           Qt::Orientation orientation,
+                                           int role) const {
   if (role == Qt::DisplayRole) {
     if (orientation == Qt::Vertical) {
       // reverse row numbers
@@ -84,32 +238,47 @@ QVariant StrobeLengthModel::headerData(int section, Qt::Orientation orientation,
   return QAbstractTableModel::headerData(section, orientation, role);
 }
 
-void StrobeLengthModel::updateModelData(quint16 (*data)[strobs]) {
+void StrobeLengthReadModel::compareData() {
+  for (int row = 0; row < _rows; ++row)
+    for (int col = 0; col < _cols; ++col)
+      if (_dataForCompare[row][col] == _data[row][col])
+        _flags[row][col] = true;
+}
+
+/**
+ * @brief StrobeLengthReadModel::setModelDataForCompare
+ */
+void StrobeLengthReadModel::setModelDataForCompare(quint16 (*data)[strobs]) {
+  // Берем данные из модели StrobeLengthWriteModel для того чтобы их
+  // сравнить с считанными данными, полученными с сервера
+  for (int row = 0; row < _rows; ++row)
+    for (int col = 0; col < _cols; ++col)
+      _dataForCompare[row][col] = data[row][col];
+}
+
+/**
+ * @brief StrobeLengthReadModel::updateModelData
+ */
+void StrobeLengthReadModel::updateModelData(quint16 (*data)[strobs]) {
   /**
-    Сравнение считанных данных с выставленными данными модели
-  */
+   * Получить данные от сервера и установить их в модель
+   */
+  for (int row = 0; row < _rows; ++row)
+    for (int col = 0; col < _cols; ++col)
+      _data[row][col] = data[row][col];
+
   beginResetModel();
-  for (int _row = 0; _row < _rows; ++_row)
-    for (int _col = 0; _col < _cols; ++_col)
-      if (_data[_row][_col] == data[_row][_col]) {
-        _flags[_row][_col] = true;
-      }
+  compareData();
   endResetModel();
 }
 
-void StrobeLengthModel::getModelData(quint16 (*data)[strobs]) {
-  /**
-    Берем данные, чтобы отослать их на сервер
-    */
-  for (int _row = 0; _row < _rows; ++_row)
-    for (int _col = 0; _col < _cols; ++_col)
-      data[_row][_col] = _data[_row][_col];
-}
-
-void StrobeLengthModel::resetFlags() {
+/**
+ * @brief StrobeLengthReadModel::resetFlags
+ */
+void StrobeLengthReadModel::resetFlags() {
   beginResetModel();
-  for (int _row = 0; _row < _rows; ++_row)
-    for (int _col = 0; _col < _cols; ++_col)
-      _flags[_row][_col] = false;
+  for (int row = 0; row < _rows; ++row)
+    for (int col = 0; col < _cols; ++col)
+      _flags[row][col] = false;
   endResetModel();
 }
--- tablemodel.h
+++ tablemodel.h
@@ -2,11 +2,13 @@
 #define TABLEMODEL_H
 
 #include <QAbstractTableModel>
+#include <QTableView>
 
 const int strobs = 14;
 const int regims = 25;
 
-class StrobeLengthModel : public QAbstractTableModel {
+// Модель данных для засылки расстановок
+class StrobeLengthWriteModel : public QAbstractTableModel {
 private:
   int _rows;
   int _cols;
@@ -15,7 +17,7 @@ private:
   bool _flags[regims][strobs];
 
 public:
-  StrobeLengthModel(QObject *parent);
+  StrobeLengthWriteModel(QObject *parent);
   QVariant data(const QModelIndex &, int) const;
   int rowCount(const QModelIndex &) const;
   int columnCount(const QModelIndex &) const;
@@ -23,8 +25,31 @@ public:
   Qt::ItemFlags flags(const QModelIndex &) const;
   QVariant headerData(int section, Qt::Orientation orientation,
                       int role = Qt::DisplayRole) const;
-  void updateModelData(quint16 (*data)[strobs]);
   void getModelData(quint16 (*data)[strobs]);
+};
+
+// Модель данных для чтения расстановок
+class StrobeLengthReadModel : public QAbstractTableModel {
+private:
+  int _rows;
+  int _cols;
+
+  quint16 _data[regims][strobs];
+  quint16 _dataForCompare[regims][strobs];
+  bool _flags[regims][strobs];
+
+  void compareData();
+
+public:
+  StrobeLengthReadModel(QObject *parent);
+  QVariant data(const QModelIndex &, int) const;
+  int rowCount(const QModelIndex &) const;
+  int columnCount(const QModelIndex &) const;
+  Qt::ItemFlags flags(const QModelIndex &) const;
+  QVariant headerData(int section, Qt::Orientation orientation,
+                      int role = Qt::DisplayRole) const;
+  void updateModelData(quint16 (*data)[strobs]);
+  void setModelDataForCompare(quint16 (*data)[strobs]);
   void resetFlags();
 };
 
